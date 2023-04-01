a19c213ce3ab42a5bb41e11c2a98e5b582641f8c
Dario Cline <darioclinemp3@gmail.com>
2023-04-01 18:49:00 +0100

    message generator works well now

--- MessageGenerator.py
+++ MessageGenerator.py
@@ -2,3 +2,16 @@ import openai
 
 def askGPT(text):
     openai.api_key = "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"
+    response = openai.Completion.create(
+    engine = "text-davinci-003",
+    prompt = text,
+    temperature = 0.6,
+    max_tokens = 150,
+    )
+    return print(response.choices[0].text)
+
+def main():
+    input = "write a rude message to gaslight people into being eco friendly"
+    askGPT(input)
+
+main()
